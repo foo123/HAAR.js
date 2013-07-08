5d8c2a7d336ff3c03c30cff384adef212709d4d7
Nikos M <Nikos@Nikos-PC.(none)>
2013-07-08 14:04:54 +0300

    remove obsolete java tool

--- cascades/haartojsj.bat
+++
@@ -1,2 +0,0 @@
-REM %1 is opencv haar xml file name without xml extension
-java -jar haartojs.jar %1.xml %1 > %1.js
--- cascades/haartojsj.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# $1 is opencv haar xml file name without xml extension
-java -jar haartojs.jar $1.xml $1 > $1.js
