d114ed21e0a0e81129c4e537d5fdcd6478bef220
Nikos M <nikosms0@gmail.com>
2015-04-10 11:21:37 +0300

    beeld config updates

--- build.bat
+++ build.bat
@@ -8,10 +8,10 @@ REM #
 REM ###################################################
 
 REM to use the python build tool do:
-REM python %BUILDTOOLS%\Beeld.py --config ".\config.custom"
+REM python %BUILDTOOLS%\Beeld.py --config ".\beeld.config"
 
 REM to use the php build tool do:
-REM php -f %BUILDTOOLS%\Beeld.php --  --config=".\config.custom"
+REM php -f %BUILDTOOLS%\Beeld.php --  --config=".\beeld.config"
 
 REM to use the node build tool do:
-node %BUILDTOOLS%\Beeld.js  --config ".\config.custom"
+node %BUILDTOOLS%\Beeld.js  --config ".\beeld.config"
